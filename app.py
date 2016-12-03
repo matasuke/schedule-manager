3eab8bde5270691a16774d35ef834d49f807906c
matasukef <Matasuke@mbp2014mid.local>
2016-12-04 03:37:02 +0900

    test

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, FollowEvent, TextMessage, TextSendMessage,
+    MessageEvent, FollowEvent, TextMessage, TextSendMessage, TemplateSendMessage
 )
 import settings
 from connectDB import *
