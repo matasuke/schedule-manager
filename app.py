1d0cba47aa031ad876db9a8a107ff43681078282
matasukef <Matasuke@mbp2014mid.local>
2016-12-04 13:22:10 +0900

    test

--- app.py
+++ app.py
@@ -71,7 +71,8 @@ def hook():
     buttons_template_message = TemplateSendMessage(
         alt_text='この情報はスマートフォンからのみ観覧できます。',
         template=ButtonsTemplate(
-            text= contents,
+            title='天気予報',
+            text= "test",
             actions=[
                 URITemplateAction(
                     label='詳しく!',
