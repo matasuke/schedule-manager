8f87e6640e8f8a5864f1efd8b9272a5a9625754c
matasukef <Matasuke@mbp2014mid.local>
2016-12-04 10:23:13 +0900

    test

--- sendmsg.py
+++ sendmsg.py
@@ -13,7 +13,7 @@ def SendMsg(row):
     message3 = "出発予定時刻は" + depart_time[:2]+ "時" + depart_time[3:5] + "分です"
     message4 = "持ち物は" + brings + "です。"
     message5 = "以下に目的地までのルートを記載します。"
-    message6 = Get_Station_info("所沢駅", place, "20161221"," 1655")
+    message6 = Get_Station_info("所沢駅", "小手指駅", "20161221"," 1655")
 
     messages = message1 + "\n" + message2 + "\n" + message3 + "\n" + message4 + "\n" + message5 + "\n" + message6
 
