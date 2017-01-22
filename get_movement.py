987b95dbf6e6d25844681def9538bab494a4dc4a
matasukef <f-e@toki.waseda.jp>
2017-01-22 14:35:19 +0900

    test

--- get_movement.py
+++ get_movement.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import sys
 import time
-import pycurl
+import requests
 
 def get_diff(im_before, im_now, th):
     
@@ -18,20 +18,17 @@ def get_movement(th):
     cap = cv2.VideoCapture(0)
     ret, f_before = cap.read()
     
-    curl = pycurl.Curl()
 
     while True:
         ret, frame = cap.read()
         
         if get_diff(f_before, frame, th):
-            curl.setopt(pycurl.URL, 'https://testapps009.herokuapp.com/post')
-            curl.setopt(pycurl.CUSTOMREQUEST, 'POST')
-            curl.perform()
+            requests.post('https://testapps009.herokuapp.com/post')    
         f_before = frame
-        time.sleep(2)
+        time.sleep(3)
 
 
         
 
 if __name__ == "__main__":
-    get_movement(20000000);
+    get_movement(50000000);
