8747c30b469336a36b5cc6ae87ef0a0a248e39f2
matasukef <f-e@toki.waseda.jp>
2017-01-22 20:03:56 +0900

    test

--- app.py
+++ app.py
@@ -89,12 +89,12 @@ def hook():
     todayHM, todayYMD = getNowTimes()
     
     #connect to database
-    #db = usePSQL(settings.host, settings.db, settings.user, settings.password)
+    db = usePSQL(settings.host, settings.db, settings.user, settings.password)
     
     #fetch datas
-    #result = db.getAllAppointment(todayHM, todayYMD)
-    #message = sendAllMsg(result, todayHM)    
-    line_bot_api.push_message("U41a55a88dcc95a269aacdf0e9c112361", TextSendMessage(text="まんこ"))
+    result = db.getAllAppointment(todayHM, todayYMD)
+    message = sendAllMsg(result, todayHM)    
+    line_bot_api.push_message("U41a55a88dcc95a269aacdf0e9c112361", TextSendMessage(text=message))
 
 
 @app.route("/callback", methods=['POST'])
